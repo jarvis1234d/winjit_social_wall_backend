4e26f64217585f619ad93b7c49506fffdb0fec10
Tarun Suryawanshi <indtarun5@gmail.com>
2022-11-28 19:17:35 +0530

    basic setup done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./src/routes/index');
+const feedRouter = require('./src/routes/feed');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/feed', feedRouter);
+
+module.exports = app;
